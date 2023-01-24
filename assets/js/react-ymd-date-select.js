cf9a636389aec2d538b3c39dd47351a5d744711d
ilxumehara <umehara@ilx.local>
2023-01-24 13:53:31 +0900

    [fix]20230124PM

--- assets/js/react-ymd-date-select.js
+++ assets/js/react-ymd-date-select.js
@@ -2,7 +2,7 @@ window.addEventListener('DOMContentLoaded',function(){
   const qualiDate1 = document.getElementById('quali_date_1');
   const qualification1 = document.getElementById('qualification_1');
   qualification1.addEventListener('change', function(){
-    if(this.checked) { // ←重要なポイント！
+    if(this.checked) {
       qualiDate1.classList.remove('unvisible');
       qualiDate1.classList.add('visible');
     } else{
@@ -16,7 +16,7 @@ window.addEventListener('DOMContentLoaded',function(){
   const qualiDate2 = document.getElementById('quali_date_2');
   const qualification2 = document.getElementById('qualification_2');
   qualification2.addEventListener('change', function(){
-    if(this.checked) { // ←重要なポイント！
+    if(this.checked) {
       qualiDate2.classList.remove('unvisible');
       qualiDate2.classList.add('visible');
     } else{
@@ -30,7 +30,7 @@ window.addEventListener('DOMContentLoaded',function(){
   const qualiDate3 = document.getElementById('quali_date_3');
   const qualification3 = document.getElementById('qualification_3');
   qualification3.addEventListener('change', function(){
-    if(this.checked) { // ←重要なポイント！
+    if(this.checked) {
       qualiDate3.classList.remove('unvisible');
       qualiDate3.classList.add('visible');
     } else{
@@ -44,7 +44,7 @@ window.addEventListener('DOMContentLoaded',function(){
   const qualiDate4 = document.getElementById('quali_date_4');
   const qualification4 = document.getElementById('qualification_4');
   qualification4.addEventListener('change', function(){
-    if(this.checked) { // ←重要なポイント！
+    if(this.checked) {
       qualiDate4.classList.remove('unvisible');
       qualiDate4.classList.add('visible');
     } else{
@@ -58,7 +58,7 @@ window.addEventListener('DOMContentLoaded',function(){
   const qualiDate5 = document.getElementById('quali_date_5');
   const qualification5 = document.getElementById('qualification_5');
   qualification5.addEventListener('change', function(){
-    if(this.checked) { // ←重要なポイント！
+    if(this.checked) {
       qualiDate5.classList.remove('unvisible');
       qualiDate5.classList.add('visible');
     } else{
@@ -72,7 +72,7 @@ window.addEventListener('DOMContentLoaded',function(){
   const qualiDate6 = document.getElementById('quali_date_6');
   const qualification6 = document.getElementById('qualification_6');
   qualification6.addEventListener('change', function(){
-    if(this.checked) { // ←重要なポイント！
+    if(this.checked) {
       qualiDate6.classList.remove('unvisible');
       qualiDate6.classList.add('visible');
     } else{
@@ -86,7 +86,7 @@ window.addEventListener('DOMContentLoaded',function(){
   const qualiDate7 = document.getElementById('quali_date_7');
   const qualification7 = document.getElementById('qualification_7');
   qualification7.addEventListener('change', function(){
-    if(this.checked) { // ←重要なポイント！
+    if(this.checked) {
       qualiDate7.classList.remove('unvisible');
       qualiDate7.classList.add('visible');
     } else{
@@ -100,7 +100,7 @@ window.addEventListener('DOMContentLoaded',function(){
   const qualiDate8 = document.getElementById('quali_date_8');
   const qualification8 = document.getElementById('qualification_8');
   qualification8.addEventListener('change', function(){
-    if(this.checked) { // ←重要なポイント！
+    if(this.checked) {
       qualiDate8.classList.remove('unvisible');
       qualiDate8.classList.add('visible');
     } else{
@@ -114,7 +114,7 @@ window.addEventListener('DOMContentLoaded',function(){
   const qualiDate9 = document.getElementById('quali_date_9');
   const qualification9 = document.getElementById('qualification_9');
   qualification9.addEventListener('change', function(){
-    if(this.checked) { // ←重要なポイント！
+    if(this.checked) {
       qualiDate9.classList.remove('unvisible');
       qualiDate9.classList.add('visible');
     } else{
@@ -128,7 +128,7 @@ window.addEventListener('DOMContentLoaded',function(){
   const qualiDate10 = document.getElementById('quali_date_10');
   const qualification10 = document.getElementById('qualification_10');
   qualification10.addEventListener('change', function(){
-    if(this.checked) { // ←重要なポイント！
+    if(this.checked) {
       qualiDate10.classList.remove('unvisible');
       qualiDate10.classList.add('visible');
     } else{
@@ -142,7 +142,7 @@ window.addEventListener('DOMContentLoaded',function(){
   const qualiDate11 = document.getElementById('quali_date_11');
   const qualification11 = document.getElementById('qualification_11');
   qualification11.addEventListener('change', function(){
-    if(this.checked) { // ←重要なポイント！
+    if(this.checked) {
       qualiDate11.classList.remove('unvisible');
       qualiDate11.classList.add('visible');
     } else{
@@ -156,7 +156,7 @@ window.addEventListener('DOMContentLoaded',function(){
   const qualiDate12 = document.getElementById('quali_date_12');
   const qualification12 = document.getElementById('qualification_12');
   qualification12.addEventListener('change', function(){
-    if(this.checked) { // ←重要なポイント！
+    if(this.checked) {
       qualiDate12.classList.remove('unvisible');
       qualiDate12.classList.add('visible');
     } else{
@@ -170,7 +170,7 @@ window.addEventListener('DOMContentLoaded',function(){
   const qualiDate13 = document.getElementById('quali_date_13');
   const qualification13 = document.getElementById('qualification_13');
   qualification13.addEventListener('change', function(){
-    if(this.checked) { // ←重要なポイント！
+    if(this.checked) {
       qualiDate13.classList.remove('unvisible');
       qualiDate13.classList.add('visible');
     } else{
@@ -184,7 +184,7 @@ window.addEventListener('DOMContentLoaded',function(){
   const qualiDate13 = document.getElementById('quali_date_13');
   const qualification13 = document.getElementById('qualification_13');
   qualification13.addEventListener('change', function(){
-    if(this.checked) { // ←重要なポイント！
+    if(this.checked) {
       qualiDate13.classList.remove('unvisible');
       qualiDate13.classList.add('visible');
     } else{
@@ -196,28 +196,84 @@ window.addEventListener('DOMContentLoaded',function(){
 
 window.addEventListener('DOMContentLoaded', function(){
   const requestArea1 = document.getElementById('request_area-1');
-  const messageRequestArea1 = docoment.getElementById('message_request_area-1');
+  const messageRequestArea1 = document.getElementById('message_request_area-1');
   if(requestArea1.disabled === true){
     messageRequestArea1.classList.remove('unvisible');
-    messageRequestArea1.classList.add('visible');
-    console.log('表示');
+    console.log('1表示');
   }else{
-    messageRequestArea1.classList.remove('visible');
     messageRequestArea1.classList.add('unvisible');
-    console.log('非表示');
+    console.log('1非表示');
   }
 });
 
 window.addEventListener('DOMContentLoaded', function(){
   const requestArea2_1 = document.getElementById('request_area-2_1');
-  const messageRequestArea2_1 = docoment.getElementById('message_request_area-2_1');
+  const messageRequestArea2_1 = document.getElementById('message_request_area-2_1');
   if(requestArea2_1.disabled === true){
     messageRequestArea2_1.classList.remove('unvisible');
-    messageRequestArea2_1.classList.add('visible');
-    console.log('表示');
+    console.log('2-1表示');
   }else{
-    messageRequestArea2_1.classList.remove('visible');
     messageRequestArea2_1.classList.add('unvisible');
-    console.log('非表示');
+    console.log('2-1非表示');
+  }
+});
+
+window.addEventListener('DOMContentLoaded', function(){
+  const requestArea2_2 = document.getElementById('request_area-2_2');
+  const messageRequestArea2_2 = document.getElementById('message_request_area-2_2');
+  if(requestArea2_2.disabled === true){
+    messageRequestArea2_2.classList.remove('unvisible');
+    console.log('2-2表示');
+  }else{
+    messageRequestArea2_2.classList.add('unvisible');
+    console.log('2-2非表示');
+  }
+});
+
+window.addEventListener('DOMContentLoaded', function(){
+  const requestArea2_3 = document.getElementById('request_area-2_3');
+  const messageRequestArea2_3 = document.getElementById('message_request_area-2_3');
+  if(requestArea2_3.disabled === true){
+    messageRequestArea2_3.classList.remove('unvisible');
+    console.log('2-3表示');
+  }else{
+    messageRequestArea2_3.classList.add('unvisible');
+    console.log('2-3非表示');
+  }
+});
+
+window.addEventListener('DOMContentLoaded', function(){
+  const requestArea2_4 = document.getElementById('request_area-2_4');
+  const messageRequestArea2_4 = document.getElementById('message_request_area-2_4');
+  if(requestArea2_4.disabled === true){
+    messageRequestArea2_4.classList.remove('unvisible');
+    console.log('2-4表示');
+  }else{
+    messageRequestArea2_4.classList.add('unvisible');
+    console.log('2-4非表示');
+  }
+});
+
+window.addEventListener('DOMContentLoaded', function(){
+  const requestArea3 = document.getElementById('request_area-3');
+  const messageRequestArea3 = document.getElementById('message_request_area-3');
+  if(requestArea3.disabled === true){
+    messageRequestArea3.classList.remove('unvisible');
+    console.log('3表示');
+  }else{
+    messageRequestArea3.classList.add('unvisible');
+    console.log('3非表示');
+  }
+});
+
+window.addEventListener('DOMContentLoaded', function(){
+  const requestArea4 = document.getElementById('request_area-4');
+  const messageRequestArea4 = document.getElementById('message_request_area-4');
+  if(requestArea4.disabled === true){
+    messageRequestArea4.classList.remove('unvisible');
+    console.log('4表示');
+  }else{
+    messageRequestArea4.classList.add('unvisible');
+    console.log('4非表示');
   }
 });
