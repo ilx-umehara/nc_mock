ed59bc50c3737dbd1f0fd63c211aae620c24d1c6
ilxumehara <umehara@ilx.local>
2023-01-27 18:36:06 +0900

    [fix]年齢自動計算追加

--- assets/js/date.js
+++ assets/js/date.js
@@ -1,7 +1,15 @@
 window.addEventListener('DOMContentLoaded', function(){
-  const ouboDate = this.document.getElementById('oubo_date');
+  const ouboDate_yyyy = this.document.getElementById('oubo_date_y');
+  const ouboDate_MM = this.document.getElementById('oubo_date_M');
+  const ouboDate_dd = this.document.getElementById('oubo_date_d');
+  const ouboDate_hh = this.document.getElementById('oubo_date_h');
+  const ouboDate_mm = this.document.getElementById('oubo_date_m');
 
-  //日付を取得
+  const birthdayYYYY = document.getElementById('birthday_y');
+  const birthdayMM = document.getElementById('birthday_m');
+  const birthdayDD = document.getElementById('birthday_d');
+
+  //今日の日付を取得
   const today = new Date();
   const yyyy = today.getFullYear();
   const MM = today.getMonth() + 1;
@@ -9,6 +17,41 @@ window.addEventListener('DOMContentLoaded', function(){
   const hh = today.getHours();
   const mm = today.getMinutes();
 
-  const formatToday = yyyy + '年' + MM + '月' + dd + '日' + hh + '時' + mm + '分';
-  ouboDate.value = formatToday;
+  ouboDate_yyyy.value = yyyy;
+  ouboDate_MM.value = MM;
+  ouboDate_dd.value = dd;
+  ouboDate_hh.value = hh;
+  ouboDate_mm.value = mm;
+  const ageCalc = document.getElementsByClassName('ageCalc');
+  for(let i = 0; i < ageCalc.length; i++){
+    ageCalc[i].addEventListener('change',function(){
+      const inputBirthdayYYYY = document.getElementById('birthday_y');
+      const inputBirthdayMM = document.getElementById('birthday_m');
+      const inputBirthdayDD = document.getElementById('birthday_d');
+      const inputOuboDate_yyyy = document.getElementById('oubo_date_y');
+      const inputOuboDate_MM = document.getElementById('oubo_date_M');
+      const inputOuboDate_dd = document.getElementById('oubo_date_d');
+      // console.log(inputOuboDate.value);
+      if(inputBirthdayYYYY.value != '' && inputBirthdayMM.value != '' && inputBirthdayDD.value != ''){
+  
+        //応募日
+        const ouboDate = new Date(inputOuboDate_yyyy.value, inputOuboDate_MM.value, inputOuboDate_dd.value);
+        //応募時の誕生日を計算
+        const ouboYearsBirthday = new Date(inputOuboDate_yyyy.value, birthdayMM.value - 1, birthdayDD.value);
+
+        //年齢
+        var age = inputOuboDate_yyyy.value - birthdayYYYY.value;
+  
+        //応募時点で応募した年の誕生日を迎えていない場合
+        console.log('応募日：' + ouboDate);
+        console.log('応募年誕生日' + ouboYearsBirthday);
+        if(ouboDate < ouboYearsBirthday){
+          age--;
+        }
+        const ouboAge = document.getElementById('oubo_age');
+        ouboAge.value = age;
+      }
+      console.log(`${ageCalc[i].name}がクリックされました！`);
+    });
+  }
 });
