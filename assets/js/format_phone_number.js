2f0bffd48c0c035dda14f30be44ed9b68372180f
ilxumehara <umehara@ilx.local>
2023-02-08 14:11:33 +0900

    [fix]No.30

--- assets/js/format_phone_number.js
+++ assets/js/format_phone_number.js
@@ -48,4 +48,29 @@ window.addEventListener("load",function(){
     console.log(formattedTel);
     inputTel2.value = formattedTel;//整形した電話番号を入力欄に返す
   });
+
+  const inputTel3 = document.getElementById('tel-3');//＝電話番号の入力欄
+  if (!inputTel3){ return false;}
+  //電話番号の入力欄から離れたら発動
+  inputTel3.addEventListener('blur', () => {
+    // バリデーション関数
+    let validateTelNeo = function (value) {
+      return /^[0０]/.test(value) && libphonenumber.isValidNumber(value, 'JP');
+    }
+ 
+    // 整形関数
+    let formatTel = function (value) {
+      return new libphonenumber.AsYouType('JP').input(value);
+    }
+ 
+    const postdata = inputTel3.value;//入力した電話番号を取得
+    //入力した内容がバリデーションに引っかかったときはエラー
+    if (!validateTelNeo(postdata)) {
+      console.log('ERROR')
+      return
+    }
+    let formattedTel = formatTel(postdata);//入力した電話番号を整形
+    console.log(formattedTel);
+    inputTel3.value = formattedTel;//整形した電話番号を入力欄に返す
+  });
 });
--- assets/js/modal-oubo_detail.js
+++ assets/js/modal-oubo_detail.js
@@ -1,10 +1,10 @@
-const btnRegistNewContact = document.getElementById('btn_regist-new-contact');
-const modalRegistNewContact = document.getElementById('modal_regist-new-contact');
-const closeBtnModalRegistNewContact = document.getElementById('close-modal_regist-new-contact');
+const btnEditName = document.getElementById('base_info_name');
+const modalEditName = document.getElementById('modal_edit_name');
+const closeBtnModalEditName = document.getElementById('close-modal_edit_name');
 
 const btnShowApplHistory = document.getElementById('btn_show-appli-history');
 const modalShowApplHistory = document.getElementById('modal_show-appli-history');
-const closeBtnModalApplHistory = document.getElementById('close-modal_show-appli-history');
+// const closeBtnModalApplHistory = document.getElementById('close-modal_show-appli-history');
 
 const btnEditEntryInfo = document.getElementById('btn_edit-entry-info');
 const modalEditEntryInfo = document.getElementById('modal_edit-entry-info');
@@ -14,11 +14,17 @@ const btnSendCorrectionRequest = document.getElementById('btn_send-correction-re
 const modalSendCorrectionRequest = document.getElementById('modal_send-correction-request');
 const closeBtnModalSendCorrectionRequest = document.getElementById('close-modal_send-correction-request');
 
-const btnEditName = document.getElementById('base_info_name');
-const modalEditName = document.getElementById('modal_edit_name');
-const closeBtnModalEditName = document.getElementById('close-modal_edit_name');
+const btnRegistNewContact = document.getElementById('btn_regist-new-contact');
+const modalRegistNewContact = document.getElementById('modal_regist-new-contact');
+const closeBtnModalRegistNewContact = document.getElementById('close-modal_regist-new-contact');
 
-const closeBtn = document.querySelector('.close');
+// const closeBtn = document.querySelector('.close');
+const close1 = document.getElementById('close_1');
+const close2 = document.getElementById('close_2');
+const close3 = document.getElementById('close_3');
+const close4 = document.getElementById('close_4');
+const close5 = document.getElementById('close_5');
+
 const overlay = document.querySelector('.overlay');
 
 
@@ -52,6 +58,31 @@ btnSendCorrectionRequest.addEventListener('click', (e) => {
   overlay.classList.add('active');
 });
 
+close1.addEventListener('click', (e) => {
+  e.preventDefault();
+  modalEditName.classList.remove('active');
+  overlay.classList.remove('active');
+});
+close2.addEventListener('click', (e) => {
+  e.preventDefault();
+  modalShowApplHistory.classList.remove('active');
+  overlay.classList.remove('active');
+});
+close3.addEventListener('click', (e) => {
+  e.preventDefault();
+  modalEditEntryInfo.classList.remove('active');
+  overlay.classList.remove('active');
+});
+close4.addEventListener('click', (e) => {
+  e.preventDefault();
+  modalSendCorrectionRequest.classList.remove('active');
+  overlay.classList.remove('active');
+});
+close5.addEventListener('click', (e) => {
+  e.preventDefault();
+  modalRegistNewContact.classList.remove('active');
+  overlay.classList.remove('active');
+});
 
 closeBtnModalEditName.addEventListener('click', function(){
   modalEditName.classList.remove('active');
@@ -60,7 +91,7 @@ closeBtnModalEditName.addEventListener('click', function(){
   modalEditEntryInfo.classList.remove('active');
   modalSendCorrectionRequest.classList.remove('active');
   overlay.classList.remove('active');
-  console.log('クリックしました。');
+  console.log('closeBtnModalEditNameクリックしました。');
 });
 closeBtnModalRegistNewContact.addEventListener('click', function(){
   modalEditName.classList.remove('active');
@@ -69,17 +100,9 @@ closeBtnModalRegistNewContact.addEventListener('click', function(){
   modalEditEntryInfo.classList.remove('active');
   modalSendCorrectionRequest.classList.remove('active');
   overlay.classList.remove('active');
-  console.log('クリックしました。');
+  console.log('closeBtnModalRegistNewContactクリックしました。');
 });
-closeBtnModalApplHistory.addEventListener('click', function(){
-  modalEditName.classList.remove('active');
-  modalRegistNewContact.classList.remove('active');
-  modalShowApplHistory.classList.remove('active');
-  modalEditEntryInfo.classList.remove('active');
-  modalSendCorrectionRequest.classList.remove('active');
-  overlay.classList.remove('active');
-  console.log('クリックしました。');
-});
+
 closeBtnModalEditEntryInfo.addEventListener('click', function(){
   modalEditName.classList.remove('active');
   modalRegistNewContact.classList.remove('active');
@@ -87,7 +110,7 @@ closeBtnModalEditEntryInfo.addEventListener('click', function(){
   modalEditEntryInfo.classList.remove('active');
   modalSendCorrectionRequest.classList.remove('active');
   overlay.classList.remove('active');
-  console.log('クリックしました。');
+  console.log('closeBtnModalEditEntryInfoクリックしました。');
 });
 closeBtnModalSendCorrectionRequest.addEventListener('click', function(){
   modalEditName.classList.remove('active');
@@ -96,7 +119,7 @@ closeBtnModalSendCorrectionRequest.addEventListener('click', function(){
   modalEditEntryInfo.classList.remove('active');
   modalSendCorrectionRequest.classList.remove('active');
   overlay.classList.remove('active');
-  console.log('クリックしました。');
+  console.log('closeBtnModalSendCorrectionRequestクリックしました。');
 });
 overlay.addEventListener('click', function() {
   modalEditName.classList.remove('active');
@@ -107,5 +130,13 @@ overlay.addEventListener('click', function() {
   overlay.classList.remove('active');
   console.log('オーバーレイをクリックしました。');
 });
-
+closeBtn.addEventListener('click', function() {
+  modalEditName.classList.remove('active');
+  modalRegistNewContact.classList.remove('active');
+  modalShowApplHistory.classList.remove('active');
+  modalEditEntryInfo.classList.remove('active');
+  modalSendCorrectionRequest.classList.remove('active');
+  overlay.classList.remove('active');
+  console.log('closeBtnをクリックしました。');
+});
 
